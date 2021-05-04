d2fa35228d27509ea2517aa0d769cd1b9bb2b425
Isaque <isaque@edu.unifor.br>
2021-05-04 17:49:53 -0300

    n vai n

--- isaque.js
+++ isaque.js
@@ -1,26 +1,59 @@
 const fs = require('fs')
 
-function generateBinaryTree(tokens){
-    return handleToken(tokens)
+
+class binaryTree{
+    constructor(){
+        this.values = {};
+        this.index = 0;
+    }
+    addValues(value){
+        this.index++;
+        this.values[this.index] = value;
+        return this.index;
+    }
+    getValue (index) { return this.values[index] }
+    getRoot () { return this.index }
+    mountTree(){ return this.mountNode(this.index) }
+    mountNode(index){
+        let node = this.getValue(index)
+        if(!node) {return index}
+        if(node.left) {node.left = this.mountNode(node.left)}
+        if(node.right) {node.right = this.mountNode(node.right)}
+        return node;
+    }
 }
-function handleToken(token){
-    if(!token.inputs[0].type){ //Checagem se há uma operação após ao veriguar se no input há um objeto ou uma string
-        console.log("here")
-        return {
+function generateBinaryTree(tokens){
+    let binary_tree = new binaryTree;
+    newHandleToken(tokens, binary_tree)
+    return binary_tree
+}
+function oldHandleToken(token, tree){
+    if(!token.inputs[0].type){
+        return tree.addValues({
             value: token.type,
             params: token.params,
             left: token.inputs[0], 
             right: token.inputs[1]
-        }
+        })
     }
-    let left = handleToken(token.inputs[0])
-    let right = token.inputs[1] ? handleToken(token.inputs[1]) : undefined
-    return {
+    let left = newHandleToken(token.inputs[0], tree)
+    let right = token.inputs[1] ? newHandleToken(token.inputs[1], tree) : undefined
+    return tree.addValues({
         value: token.type,
         params: token.params,
         left, 
         right
-    }
+    })
+}
+function newHandleToken(token, tree){
+    let left = token.inputs[0].type ? newHandleToken(token.inputs[0], tree) : token.inputs[0]
+    let right = token.inputs[1] ? token.inputs[1].type ? newHandleToken(token.inputs[1], tree) : token.inputs[1] : undefined
+    return tree.addValues({
+        value: token.type,
+        params: token.params,
+        left, 
+        right
+    })
 }
 
 let select1 = {
@@ -88,7 +121,7 @@ let select3 = {
                             {
                                 type: 'projection',
                                 inputs: ["Works_On"],
-                                params: ["ESSN, PNO"]
+                                params: ["ESSN", "PNO"]
                             }
                         ],
                         params: ["PNUMBER", "PNO"]
@@ -109,14 +142,91 @@ let select3 = {
             }],
         params: ["ESSN", "SSN"] }
     ],
-    params: ["ESSN", "SSN"]
+    params: ["LNAME"]
+}
+let select3_2 = {
+    type: "projection",
+    inputs: [
+        {
+            type: "selection",
+            inputs:[{
+                type: "cartesian_product",
+                inputs:[
+                    "Project",
+                    {
+                        type: "cartesian_product",
+                        inputs:["Works_On", "Employee"],
+                        params: []
+                    }
+                ],
+                params: []
+            }],
+            params:[
+                {
+                    type: "AND",
+                    params: [
+                        {
+                            type: "EQUAL",
+                            params: ["P.NAME", "Aquarius"]
+                        },
+                        {
+                            type: "EQUAL",
+                            params: ["P.NUMBER", "PNO"]
+                        },
+                        {
+                            type: "EQUAL",
+                            params: ["ESSN", "SSN"]
+                        },
+                        {
+                            type: "GREATER_THAN",
+                            params: ["BDATE", "1957-12-31"]
+                        },
+                    ]
+                },
+            ]
+        }
+    ],
+    params: ["LNAME"]
 }
 
-let response = generateBinaryTree(select1)
-response = generateBinaryTree(select2)
-response = generateBinaryTree(select3)
+let x = generateBinaryTree(select3_2)
+console.log(x)
+let nodes_to_trade = []
+let iterator = []
+let index = x.getRoot()
+while(x.getValue(index) != null || iterator.length > 0){
+    if(x.getValue(index) != null){
+        if(x.getValue(index).value == 'projection'/* || x.getValue(index).value == 'selection'*/){
+            nodes_to_trade.push({father: iterator[iterator.length-1], node: index})
+        }
+        else if(x.getValue(index).value == 'selection'){
+            x.getValue(index).params.forEach((sel_param) => {
+                nodes_to_trade.forEach((entry) => {
+                    x.getValue(entry.node).params.forEach((proj_param) => {
+                        console.log(`${sel_param} versus ${proj_param}`)
+                        if(sel_param == proj_param){
+                            console.log("Here")
+                        }
+                    })
+                })
+            })
+        }
+        iterator.push(index)
+        index = x.getValue(index)?.left
+    }
+    else{
+        index = iterator.pop()
+        index = x.getValue(index)?.right
+    }
+}
+console.log("Nodes pra trocar:")
+console.log(nodes_to_trade)
 
-fs.writeFile("out.json", JSON.stringify(response), 'utf8', (err) => {
+
+
+//console.log(response2)
+
+/*fs.writeFile("out1.json", JSON.stringify(response), 'utf8', (err) => {
     if(err){
         console.log(err)
     }
@@ -124,4 +234,31 @@ fs.writeFile("out.json", JSON.stringify(response), 'utf8', (err) => {
         console.log("Arquivo escrito com sucesso")
     }
 })
-//console.log(response)
+response = generateBinaryTree(select2)
+fs.writeFile("out2.json", JSON.stringify(response), 'utf8', (err) => {
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("Arquivo escrito com sucesso")
+    }
+})*/
+
+let response = generateBinaryTree(select3_2)
+fs.writeFile("out3_2_tree.json", JSON.stringify(response), 'utf8', (err) => {
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("Arquivo escrito com sucesso")
+    }
+})
+response = response.mountTree()
+fs.writeFile("out3_2_mounted_tree.json", JSON.stringify(response), 'utf8', (err) => {
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("Arquivo escrito com sucesso")
+    }
+})
